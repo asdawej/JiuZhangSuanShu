65e435cad214743a32b868d0d27dddfa2ceab172
964293341 <asdawej123@163.com>
2025-03-17 23:37:23 +0800

    简化了`int`转化为`分数`

--- 方田_分数.py
+++ 方田_分数.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from functools import total_ordering
+from typing import Literal
 
 d_汉字数字 = {0: '零', 1: '一', 2: '二', 3: '三', 4: '四', 5: '五', 6: '六', 7: '七', 8: '八', 9: '九'}
 
@@ -22,7 +23,7 @@ def 约分术(分子: int, 分母: int) -> tuple[int, int]:
 
 @total_ordering
 class 分数:
-    def __init__(self, 分子: int, 分母: int):
+    def __init__(self, 分子: int, 分母: int = 1) -> None:
         self.分子, self.分母 = 约分术(分子, 分母)
 
     def __str__(self) -> str:
@@ -41,19 +42,19 @@ class 分数:
 
     def __sub__(self, x: 分数 | int) -> 分数:
         if isinstance(x, int):
-            x = 分数(x, 1)
+            x = 分数(x)
         return 分数(abs(self.分子 * x.分母 - self.分母 * x.分子), self.分母 * x.分母)
 
     def __truediv__(self, x: 分数 | int) -> 分数:
         if isinstance(x, int):
-            x = 分数(x, 1)
+            x = 分数(x)
         if x.分子 == 0:
             raise ZeroDivisionError
         return self * 分数(x.分母, x.分子)
 
     def __floordiv__(self, x: 分数 | int) -> 分数:
         if isinstance(x, int):
-            x = 分数(x, 1)
+            x = 分数(x)
         if x.分子 == 0:
             raise ZeroDivisionError
         return 分数((self.分子 * x.分母) // (self.分母 * x.分子), 1)
@@ -61,23 +62,23 @@ class 分数:
     def __mod__(self, x: 分数 | int) -> 分数:
         return self - self // x * x
 
-    def __divmod__(self, x: 分数 | int) -> 分数:
+    def __divmod__(self, x: 分数 | int) -> tuple[分数, 分数]:
         return self // x, self % x
 
     def __lt__(self, x: 分数 | int) -> bool:
         if isinstance(x, int):
-            x = 分数(x, 1)
+            x = 分数(x)
         return self.分子 * x.分母 - self.分母 * x.分子 < 0
 
     def __eq__(self, x: 分数 | int) -> bool:
         if isinstance(x, int):
-            x = 分数(x, 1)
+            x = 分数(x)
         return self.分子 * x.分母 - self.分母 * x.分子 == 0
 
 
 @total_ordering
 class 面积:
-    def __init__(self, 顷: 分数, 积里: 分数, 亩: 分数, 积步: 分数):
+    def __init__(self, 顷: 分数, 积里: 分数, 亩: 分数, 积步: 分数) -> None:
         self.顷 = 顷
         self.积里 = 积里
         self.亩 = 亩
@@ -86,16 +87,16 @@ class 面积:
     def __str__(self) -> str:
         return f'{self.顷}顷{self.积里}里{self.亩}亩{self.积步}步'
 
-    def 换算(self, base=None):
+    def 换算(self, base: Literal['步'] | None = None) -> None:
         self.积步 += self.顷 * 100 * 240 + self.亩 * 240 + self.积里 * 90000
         if base == '积步':
-            self.顷 = 分数(0, 1)
-            self.积里 = 分数(0, 1)
-            self.顷 = 分数(0, 1)
+            self.顷 = 分数(0)
+            self.积里 = 分数(0)
+            self.顷 = 分数(0)
         else:
             self.亩, self.积步 = divmod(self.积步, 240)
             self.顷, self.亩 = divmod(self.亩, 100)
-            self.积里 = 分数(0, 1)
+            self.积里 = 分数(0)
 
     def __mul__(self, x: 分数) -> 面积:
         s = 面积(顷=self.顷 * x, 积里=self.积里 * x, 亩=self.亩 * x, 积步=self.积步 * x)
@@ -103,12 +104,12 @@ class 面积:
         return s
 
     def __add__(self, x: 面积) -> 面积:
-        s = 面积(self.顷 + x.顷, self.积里 + x.积里, self.亩 + x.亩, self.积步 + x.积步)
+        s = 面积(顷=self.顷 + x.顷, 积里=self.积里 + x.积里, 亩=self.亩 + x.亩, 积步=self.积步 + x.积步)
         s.换算()
         return s
 
     def __sub__(self, x: 面积) -> 面积:
-        s = 面积(self.顷 - x.顷, self.积里 - x.积里, self.亩 - x.亩, self.积步 - x.积步)
+        s = 面积(顷=self.顷 - x.顷, 积里=self.积里 - x.积里, 亩=self.亩 - x.亩, 积步=self.积步 - x.积步)
         s.换算()
         return s
 
@@ -125,13 +126,13 @@ class 面积:
 
 @total_ordering
 class 长度:
-    def __init__(self, 里: 分数, 步: 分数):
+    def __init__(self, 里: 分数, 步: 分数) -> None:
         self.里 = 里
         self.步 = 步
 
-    def 换算(self, base=None):
+    def 换算(self, base: Literal['步'] | None = None) -> None:
         if base == '步':
-            self.里, self.步 = 分数(0, 1), self.里 * 300 + self.步
+            self.里, self.步 = 分数(0), self.里 * 300 + self.步
         else:
             self.里, self.步 = divmod(self.里 * 300 + self.步, 300)
 
@@ -143,23 +144,23 @@ class 长度:
             s = 长度(里=self.里 * x, 步=self.步 * x)
             s.换算()
             return s
-        if self.步 == 分数(0, 1) and x.步 == 分数(0, 1):
-            s = 面积(顷=分数(0, 1), 积里=分数(0, 1), 亩=self.里 * x.里 * 375, 积步=分数(0, 1))
+        if self.步 == 分数(0) and x.步 == 分数(0):
+            s = 面积(顷=分数(0), 积里=分数(0), 亩=self.里 * x.里 * 375, 积步=分数(0))
             s.换算()
             return s
         self.换算(base='步')
         x.换算(base='步')
-        s = 面积(顷=分数(0, 1), 积里=分数(0, 1), 亩=分数(0, 1), 积步=self.步 * x.步)
+        s = 面积(顷=分数(0), 积里=分数(0), 亩=分数(0), 积步=self.步 * x.步)
         s.换算()
         return s
 
     def __add__(self, x: 长度) -> 长度:
-        s = 长度(self.里 + x.里, self.步 + x.步)
+        s = 长度(里=self.里 + x.里, 步=self.步 + x.步)
         s.换算()
         return s
 
     def __sub__(self, x: 长度) -> 长度:
-        s = 长度(self.里 - x.里, self.步 - x.步)
+        s = 长度(里=self.里 - x.里, 步=self.步 - x.步)
         s.换算()
         return s
 
@@ -191,8 +192,7 @@ def 平分术(l: list[分数]) -> tuple[list[tuple[str, 分数]], 分数]:
     列数 = len(l)
     列实 = [x * 列数 for x in 未并者]
     新分母 = 列数 * 法
-    d = {True: '减', False: '益'}
-    余 = [(d[平实 < x], abs(平实 - x)) for x in 列实]
+    余 = [('减' if 平实 < x else '益', abs(平实 - x)) for x in 列实]
     减益 = [(x[0], 分数(x[1], 新分母)) for x in 余]
     平 = 分数(平实, 法)
     return 减益, 平
